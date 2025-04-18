6bfecbf225b437073d79de3b409735ed5b2912ef
Chris Maddox <chris@juniperplatform.com>
2025-04-18 13:19:46 -0400

    maybe this works for models?

--- fastapi_users/schemas.py
+++ fastapi_users/schemas.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any, Dict, Generic, List, Optional, Type, TypeVar
 
 from pydantic import BaseModel, ConfigDict, EmailStr
@@ -5,20 +6,22 @@ from pydantic.version import VERSION as PYDANTIC_VERSION
 
 from fastapi_users import models
 
+pydantic_v1_override = os.environ.get("FASTAPI_PYDANTIC_FORCE_V2", True)
+
 PYDANTIC_V2 = PYDANTIC_VERSION.startswith("2.")
 
-SCHEMA = TypeVar("SCHEMA", bound=BaseModel)
-
 if PYDANTIC_V2:  # pragma: no cover
+    from pydantic.v1 import BaseModel as BaseModelV1
+    SCHEMA = TypeVar("SCHEMA", bound=BaseModelV1)
 
-    def model_dump(model: BaseModel, *args, **kwargs) -> Dict[str, Any]:
+    def model_dump(model: BaseModelV1, *args, **kwargs) -> Dict[str, Any]:
         return model.model_dump(*args, **kwargs)  # type: ignore
 
     def model_validate(schema: Type[SCHEMA], obj: Any, *args, **kwargs) -> SCHEMA:
         return schema.model_validate(obj, *args, **kwargs)  # type: ignore
 
 else:  # pragma: no cover  # type: ignore
-
+    SCHEMA = TypeVar("SCHEMA", bound=BaseModel)
     def model_dump(model: BaseModel, *args, **kwargs) -> Dict[str, Any]:
         return model.dict(*args, **kwargs)  # type: ignore
 
@@ -53,12 +56,8 @@ class BaseUser(CreateUpdateDictModel, Generic[models.ID]):
     is_superuser: bool = False
     is_verified: bool = False
 
-    if PYDANTIC_V2:  # pragma: no cover
-        model_config = ConfigDict(from_attributes=True)  # type: ignore
-    else:  # pragma: no cover
-
-        class Config:
-            orm_mode = True
+    class Config:
+        orm_mode = True
 
 
 class BaseUserCreate(CreateUpdateDictModel):
@@ -93,12 +92,8 @@ class BaseOAuthAccount(BaseModel, Generic[models.ID]):
     account_id: str
     account_email: str
 
-    if PYDANTIC_V2:  # pragma: no cover
-        model_config = ConfigDict(from_attributes=True)  # type: ignore
-    else:  # pragma: no cover
-
-        class Config:
-            orm_mode = True
+    class Config:
+        orm_mode = True
 
 
 class BaseOAuthAccountMixin(BaseModel):
